3b2bb503cd68b9e204c8fb0b2a65c463d2e0d456
Fabián Guidobaldi <guidobaldifabian@gmail.com>
2020-11-23 19:21:37 -0300

    Add Observer comments

--- DesignPatterns/Observer/Pregunta.cs
+++ DesignPatterns/Observer/Pregunta.cs
@@ -18,6 +18,10 @@ namespace DesignPatterns.Observer
             Respuestas = new List<string>();
         }
 
+        /* Sin la utilización del patrón Observer, la clase Pregunta tendría dependencias con todos los tipos de observadores (y viceversa).
+         * Sin embargo, a partir de este patrón, tanto los Sujetos como los Observadores implementan su respectiva interfaz, con lo cual se
+         * eliminan estas dependencias. */
+
         public void AgregarRespuesta(string resp)
         {
             Respuestas.Add(resp);
@@ -27,6 +31,10 @@ namespace DesignPatterns.Observer
         public void Notificar()
         {
             Seguidores.ForEach(obs => obs.Actualizar(Respuestas[Respuestas.Count -1], Id)); // Estructura PUSH
+            
+            /* En una estructura PUSH, cuando se notifica a todos los observadores, se les envían los datos correspondientes al evento
+            * que origina la notificación. En una estructura PULL, en cambio, sólo se informa de este evento, y son los observadores los
+            * encargados de solicitar los datos que necesitan para realizar sus operaciones. */
         }
 
         public void Suscribir(IObservador observador)
@@ -40,5 +48,7 @@ namespace DesignPatterns.Observer
             Seguidores.Remove(observador);
             Console.WriteLine(observador + " se desuscribió de la pregunta " + Convert.ToString(Id));
         }
+
+        /* Utilizando el patrón Observer, es muy sencillo suscribir y desuscribir nuevos observadores manteniendo pocas dependencias. */
     }
 }
--- DesignPatterns/Observer/Usuario.cs
+++ DesignPatterns/Observer/Usuario.cs
@@ -4,6 +4,8 @@ namespace DesignPatterns.Observer
 {
     public class Usuario : IObservador
     {
+        /* Utilizando el patrón Observer, como todos los observadores implementan la misma interfaz, para agregar un nuevo tipo de observador
+         * sólo basta con implementar la interfaz, eliminando las dependencias con los sujetos a los cuales se suscriben. */
         public string Nickname { get; set; }
 
         public Usuario(string nick)
